d04dd727e9a08fbbb9f00a86e079bbbc43f4ae02
yukamatsubara23 <yukamatsubara23@gmail.com>
2024-11-04 00:46:04 +0900

    kadai_007の提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang ="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>PHP連想配列</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $vegetable = ['name' => 'onion', 'price'=> 200, 'weight'=> 160];
+
+    // 連想配列の値を出力する
+    print_r($vegetable);
+    ?>
+  </p>
+</body>
+
+</html>
